e08858f40440643836e037148e3cd8056e6d107c
Rishabh-Prajapati-creator <rishabhprajapati1470@gmail.com>
2023-11-19 11:24:40 +0530

    Fixed NewsApi

--- src/Components/News.js
+++ src/Components/News.js
@@ -35,7 +35,7 @@ export class News extends Component {
 
   async updateNews() {
     // this.props.setProgress(10);
-    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=5fec244e2a6d46d3aff2bb2e3e4f952e&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     this.setState({ loading: true })
     let data = await fetch(url);
     // this.props.setProgress(30);
